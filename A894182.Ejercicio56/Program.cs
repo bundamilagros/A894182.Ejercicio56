561d0d720d610505e01c36ba19545dc7722e0913
milagrosbunda <milagrosbunda@hotmail.com>
2021-03-19 13:17:19 -0300

    Agregar archivos de proyecto.

---
+++ A894182.Ejercicio56/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A894182.Ejercicio56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
